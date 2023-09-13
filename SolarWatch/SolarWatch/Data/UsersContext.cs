39c8aeb36252c7f8dbeca68f1b7137d55d323a17
Gulyas Mate <mategulyas01@gmail.com>
2023-09-13 22:09:58 +0200

    register the new UsersContext as a DbContext

---
+++ SolarWatch/SolarWatch/Data/UsersContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SolarWatch.Data
+{
+    public class UsersContext : IdentityUserContext<IdentityUser>
+    {
+        public UsersContext(DbContextOptions<UsersContext> options)
+            : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // It would be a good idea to move the connection string to user secrets
+            options.UseSqlServer("Server=localhost,1433;Database=SolarWatchApi;User Id=sa;Password=yourStrong(!)Password;Encrypt=True;TrustServerCertificate=True;");
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- SolarWatch/SolarWatch/Program.cs
+++ SolarWatch/SolarWatch/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using SolarWatch.Data;
 using SolarWatch.Services;
 using SolarWatch.Services.Json;
 using SolarWatch.Services.Repositories;
@@ -10,6 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 AddAuthentication();
 
+builder.Services.AddDbContext<UsersContext>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
