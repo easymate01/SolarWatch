53d9f56efedae8ece29a149738212b13ac1f2b11
Gulyas Mate <mategulyas01@gmail.com>
2023-09-26 17:33:20 +0200

    2 tests been written.

--- SolarWatch/SolarWatchTest/WetherProviderTest.cs
+++ SolarWatch/SolarWatchTest/WetherProviderTest.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Moq;
 using SolarWatch.Controllers;
-using SolarWatch.Models;
+using SolarWatch.Models.Cities;
 using SolarWatch.Models.SunriseSunset;
 using SolarWatch.Services;
 using SolarWatch.Services.Json;
@@ -32,67 +32,80 @@ namespace SolarWatchTest
                 _cityRepository.Object, _sunriseSunsetRepository.Object);
         }
 
-        [Test]
-        public async Task GetLatLonReturnsNotFoundResultIfWeatherDataProviderFails()
-        {
-            var weatherData = "{}";
-            _weatherDataProviderMock.Setup(x => x.GetLatLon(It.IsAny<string>())).ThrowsAsync(new Exception());
-
-
-            var result = await _controller.GetSunriseSunset(It.IsAny<string>(), It.IsAny<DateTime>());
-
-            Assert.IsInstanceOf(typeof(NotFoundObjectResult), result.Result);
-        }
 
         [Test]
-        public async Task GetSunriseSunsetReturnsNotFoundResultIfWeatherDataProviderFails()
-        {
-            var weatherData = "{}";
-            _weatherDataProviderMock.Setup(x =>
-                    x.GetSunriseSunset(It.IsAny<double>(), It.IsAny<double>(), It.IsAny<DateTime>()))
-                    .ThrowsAsync(new Exception());
-
-
-            var result = await _controller.GetSunriseSunset(It.IsAny<string>(), It.IsAny<DateTime>());
-
-            Assert.IsInstanceOf(typeof(NotFoundObjectResult), result.Result);
-        }
-
-
-
-        [Test]
-        public async Task GetLatLontReturnsNotFoundResultIfWeatherDataIsInvalid()
+        public async Task GetSunriseSunset_CityExists_ReturnsOkResult()
         {
             // Arrange
-            var expectedResponse = new GeocodingApiResponse();
-            var weatherData = "{}";
-            _weatherDataProviderMock.Setup(x => x.GetLatLon(It.IsAny<string>())).ReturnsAsync(weatherData);
-            _jsonProcessorMock.Setup(x => x.GetGeocodingApiResponse(weatherData)).Returns(expectedResponse);
+            var cityName = "ExistingCity";
+            var date = DateTime.Now.Date;
+            var existingCity = new City { Name = cityName }; // Create an existing city
+            var sunriseSunsetData = new SunriseSunsetResults { /* Initialize with data */ };
+
+            _cityRepository.Setup(x => x.GetByName(cityName)).Returns(existingCity);
+            _sunriseSunsetRepository.Setup(x => x.GetByCityAndDateAsync(existingCity.Id, date.Date))
+                .ReturnsAsync(sunriseSunsetData);
 
             // Act
-            var result = await _controller.GetSunriseSunset(It.IsAny<string>(), It.IsAny<DateTime>());
+            var result = await _controller.GetSunriseSunset(cityName, date);
 
             // Assert
-            Assert.IsInstanceOf(typeof(NotFoundObjectResult), result.Result);
+            Assert.IsInstanceOf<OkObjectResult>(result.Result);
+            var okResult = (OkObjectResult)result.Result;
+            Assert.AreEqual(sunriseSunsetData, okResult.Value); // Ensure the expected data is returned
         }
 
         [Test]
-        public async Task GetSunriseSunsettReturnsNotFoundResultIfWeatherDataIsInvalid()
+        public async Task GetSunriseSunset_CityExists_SunriseSunsetDataMissing_ExternalAPICallFails_ReturnsNotFound()
         {
             // Arrange
-            var expectedResponse = new SunriseSunsetResults();
-            var weatherData = "{}";
+            var cityName = "ExistingCity";
+            var date = DateTime.Now.Date;
+            var existingCity = new City { Name = cityName };
 
-            _weatherDataProviderMock.Setup(x =>
-                x.GetSunriseSunset(It.IsAny<double>(), It.IsAny<double>(), It.IsAny<DateTime>())).ReturnsAsync(weatherData);
-
-            _jsonProcessorMock.Setup(x => x.Process(weatherData, It.IsAny<string>(), It.IsAny<DateTime>())).Returns(expectedResponse);
+            _cityRepository.Setup(x => x.GetByName(cityName)).Returns(existingCity);
+            _sunriseSunsetRepository.Setup(x => x.GetByCityAndDateAsync(existingCity.Id, date.Date)).ReturnsAsync((SunriseSunsetResults)null);
+            _weatherDataProviderMock.Setup(x => x.GetSunriseSunset(It.IsAny<double>(), It.IsAny<double>(), It.IsAny<DateTime>())).ThrowsAsync(new Exception()); // Simulate an API call failure
 
             // Act
-            var result = await _controller.GetSunriseSunset(It.IsAny<string>(), It.IsAny<DateTime>());
+            var result = await _controller.GetSunriseSunset(cityName, date);
 
             // Assert
-            Assert.IsInstanceOf(typeof(NotFoundObjectResult), result.Result);
+            Assert.IsInstanceOf<NotFoundObjectResult>(result.Result);
         }
+
+
+        //[Test]
+        //public async Task GetSunriseSunset_ReturnsOkResult_WhenWeatherDataIsValid()
+        //{
+        //    // Arrange
+        //    var cityName = "Budapest";
+        //    var date = DateTime.Now.Date;
+        //    var expectedSunriseSunset = new SunriseSunsetResults { /* Initialize with valid data */ };
+        //    var weatherData = "{}";
+
+        //    _cityRepository.Setup(x => x.GetByName(cityName)).Returns(new City { Name = cityName });
+        //    _sunriseSunsetRepository.Setup(x => x.GetByCityAndDateAsync(It.IsAny<int>(), date.Date))
+        //        .ReturnsAsync((SunriseSunsetResults)null); // Simulate sunrise and sunset data missing in repository
+        //    _weatherDataProviderMock.Setup(x => x.GetLatLon(cityName)).ReturnsAsync(weatherData);
+        //    _jsonProcessorMock.Setup(x => x.GetGeocodingApiResponse(weatherData))
+        //        .Returns(new GeocodingApiResponse { Coord = new Coordinates { Lat = 12.34, Lon = 56.78 } });
+        //    _weatherDataProviderMock.Setup(x => x.GetSunriseSunset(12.34, 56.78, date))
+        //        .ReturnsAsync("{}"); // Simulate valid weather data
+        //    _jsonProcessorMock.Setup(x => x.Process("{}", cityName, date)).Returns(expectedSunriseSunset);
+
+        //    // Act
+        //    var result = await _controller.GetSunriseSunset(cityName, date);
+
+
+        //    // Assert
+        //    Assert.IsInstanceOf<OkObjectResult>(result.Result);
+        //    var okResult = (OkObjectResult)result.Result;
+        //    Assert.AreEqual(expectedSunriseSunset, okResult.Value);
+        //}
+
+
+
+
     }
 }
